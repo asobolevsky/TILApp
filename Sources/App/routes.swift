855a9f4caed74461375a10b019162eb783757d97
Aleksei Sobolevskii <aleksei.sobolevskii@sentiance.com>
2020-09-30 09:25:42 -0400

    Add CRUD operations for Acronym

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -19,4 +19,34 @@ func routes(_ app: Application) throws {
         }
     }
 
+    app.get("api", "acronyms") { req -> EventLoopFuture<[Acronym]> in
+        return Acronym.query(on: req.db).all()
+    }
+    
+    app.get("api", "acronyms", ":acronymID") { req -> EventLoopFuture<Acronym> in
+        return Acronym.find(req.parameters.get("acronymID"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+    }
+    
+    app.put("api", "acronyms", ":acronymID") { req -> EventLoopFuture<Acronym> in
+        let updatedAcronym = try req.content.decode(Acronym.self)
+        return Acronym.find(req.parameters.get("acronymID"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap { acronym in
+                acronym.short = updatedAcronym.short
+                acronym.long = updatedAcronym.long
+                return acronym.save(on: req.db).map {
+                    acronym
+                }
+            }
+    }
+    
+    app.delete("api", "acronyms", ":acronymID") { req -> EventLoopFuture<HTTPStatus> in
+        return Acronym.find(req.parameters.get("acronymID"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap { acronym in
+                acronym.delete(on: req.db)
+                    .transform(to: .noContent)
+            }
+    }
 }
